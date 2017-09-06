8515bcf885318a3c214ed59bd62e09388aa68d8c
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-09-06 09:12:37 +0800

    Reboot/Recovery QS Tile xyyx: Adapted and reworked to O qjohn: Vectorized icons aleha.druga: Adapted and reworked to P
    Change-Id: Ie7944f7994b623665015010767142ca5190b5cb6

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -78,6 +79,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AirplaneModeTile(mHost);
             case "work":
                 return new WorkModeTile(mHost);
+            case "reboot":
+                 return new RebootTile(mHost);
             case "rotation":
                 return new RotationLockTile(mHost);
             case "flashlight":
