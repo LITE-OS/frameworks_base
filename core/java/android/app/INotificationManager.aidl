DirtyUnicorns/android_frameworks_base

Allow to suppress notifications sound/vibration if screen is on [1/2]
Change-Id: Iaf5b10d84706c425e850238734f29e9fb9df6bf9

core/java/android/app/INotificationManager.aidl
@@ -148,4 +148,6 @@ interface INotificationManager
     void applyRestore(in byte[] payload, int user);
 
     ParceledListSlice getAppActiveNotifications(String callingPkg, int userId);
+
+    void setMediaPlaying(boolean playing);
 }

core/java/android/app/NotificationManager.java
@@ -1219,4 +1219,13 @@ public class NotificationManager {
         }
     }
 
+    /** @hide */
+    public void setMediaPlaying(boolean playing) {
+        final INotificationManager service = getService();
+        try {
+            service.setMediaPlaying(playing);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
 }

LITE-OS/frameworks_base

core/java/android/app/INotificationManager.aidl
@@ -148,4 +148,6 @@ interface INotificationManager
     void applyRestore(in byte[] payload, int user);
 
     ParceledListSlice getAppActiveNotifications(String callingPkg, int userId);
+
+    void setTrackPlaying(boolean playing);
 }

core/java/android/app/NotificationManager.java
@@ -1219,4 +1219,13 @@ public class NotificationManager {
         }
     }
 
+    /** @hide */
+    public void setTrackPlaying(boolean playing) {
+        final INotificationManager service = getService();
+        try {
+            service.setTrackPlaying(playing);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
 }