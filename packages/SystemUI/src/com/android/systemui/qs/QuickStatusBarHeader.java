83a4237766da26b2029df730ed66df9bae895ea8
IacobIonut01 <paulionut2003@gmail.com>
2018-07-03 23:12:17 +0300

    base: Rearrange QSPanel UI
    Change-Id: I456bf97bac35a76662aa5c00f503e0ef07c8c8b9
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -65,8 +65,6 @@ public class QuickStatusBarHeader extends RelativeLayout {
     private Clock mLeftClock;
 
 	private View mDate;
-	private View mHeaderView;
-	private String mHeaderColor = "#00FFFFFF";
 
     public QuickStatusBarHeader(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -90,9 +88,6 @@ public class QuickStatusBarHeader extends RelativeLayout {
         Rect tintArea = new Rect(0, 0, 0, 0);
         mBatteryView = findViewById(R.id.battery);
         mBatteryView.setIsQuickSbHeaderOrKeyguard(true);
-		
-		mHeaderView = findViewById(R.id.qs_top_background);
-		updateColor();
 
         mClock = findViewById(R.id.clock);
         ((Clock)mClock).setIsQshb(true);
@@ -114,30 +109,6 @@ public class QuickStatusBarHeader extends RelativeLayout {
         applyDarkness(R.id.clock, tintArea, intensity, colorForeground);
 		applyDarkness(R.id.left_clock, tintArea, intensity, colorForeground);
 		applyDarkness(R.id.date, tintArea, intensity, colorForeground);
-    }
-	
-	private class ColorObserver extends ContentObserver {
-        ColorObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            getContext().getContentResolver().registerContentObserver(Settings.System
-                    .getUriFor(Settings.System.QS_HEADER_COLOR), false,
-                    this, UserHandle.USER_ALL);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            updateColor();
-        }
-    }
-	
-	private void updateColor() {
-        mHeaderColor = Settings.System.getStringForUser(getContext().getContentResolver(),
-                Settings.System.QS_HEADER_COLOR,
-                UserHandle.USER_CURRENT);
-		mHeaderView.setBackgroundColor(Color.parseColor(mHeaderColor));
     }
 
     public void updateBatterySettings() {
